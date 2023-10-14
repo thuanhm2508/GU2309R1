34d4a13b90e1bb5c3decd95745dbe25528cabe3c
thuanhm2508 <thuanhm2508@gmail.com>
2023-10-14 20:42:25 +0700

    BT03

---
+++ GU1-W02/BT03 - Hien thi loi chao/BT04 - Hien thi loi chao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT04___Hien_thi_loi_chao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
