46e51e5550910e9fa8cd74f3287fac3638411a5a
thuanhm2508 <thuanhm2508@gmail.com>
2023-10-14 19:42:35 +0700

    BT02 - Su dung toan tu
    GU1 - W02 : BT02 Su dung toan tu

---
+++ GU1-W02/BT01 - System Time/BT02-Su dung toan tu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT02_Su_dung_toan_tu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // khai báo các biến chiều cao và chiều rộng
+            float width;
+            float height;
+            // Lấy dữ liệu người dùng
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            // Tính diện tích của hình chữ nhật và hiển thị ra màn hình
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+
+        }
+    }
+}
