d57ca0c70dd780744599a785107cb64a67accb66
thuanhm2508 <thuanhm2508@gmail.com>
2023-10-15 09:59:18 +0700

    T

--- GU1-W02/BT04 - Ung dung chuyen doi tien te/BT04 Ung dung chuyen doi tien te/Program.cs
+++ GU1-W02/BT04 - Ung dung chuyen doi tien te/BT04 Ung dung chuyen doi tien te/Program.cs
@@ -11,61 +11,11 @@ namespace BT04_Ung_dung_chuyen_doi_tien_te
 
         static void Main(string[] args)
         {
-            int choice;
-            Console.WriteLine("Menu");
-            Console.WriteLine("1. Draw the triangle");
-            Console.WriteLine("2. Draw the square");
-            Console.WriteLine("3. Draw the rectangle");
-            Console.WriteLine("0. Exit");
-            Console.Write("Enter your choice: ");
-
-            choice = Int32.Parse(Console.ReadLine());
-
-            while (choice != 0)
-            {
-                switch (choice)
-                {
-                    case 1:
-                        Console.WriteLine("Draw the triangle");
-                        Console.WriteLine("******");
-                        Console.WriteLine("*****");
-                        Console.WriteLine("****");
-                        Console.WriteLine("***");
-                        Console.WriteLine("**");
-                        Console.WriteLine("*");
-                        break;
-                    case 2:
-                        Console.WriteLine("Draw the square");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        break;
-                    case 3:
-                        Console.WriteLine("Draw the rectangle");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        break;
-                    case 0:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("No choice!");
-                        break;
-                }
-                Console.WriteLine("Menu");
-                Console.WriteLine("1. Draw the triangle");
-                Console.WriteLine("2. Draw the square");
-                Console.WriteLine("3. Draw the rectangle");
-                Console.WriteLine("0. Exit");
-                Console.Write("Enter your choice: ");
-                choice = Int32.Parse(Console.ReadLine());
-            }
-
-
+           int a = 1; b = 0;
+            Console.WriteLine("Nhập vào USD : ", a);
         }
+
+
     }
+}
 }
---
+++ GU1-W02/BT04 - Ung dung chuyen doi tien te/BT04-UD Chuyen Doi Tien Te/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT04_UD_Chuyen_Doi_Tien_Te
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int USD;
+            Console.Write("Nhap so tien USD: ");
+            USD = int.Parse(Console.ReadLine());
+            Console.WriteLine("So tien VND la: " + USD * 23000);
+
+        }
+    }
+}
