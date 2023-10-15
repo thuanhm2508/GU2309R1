9c57436d90082a76087e8e3b26ec38962e0cfb61
thuanhm2508 <thuanhm2508@gmail.com>
2023-10-15 11:37:10 +0700

    A1

---
+++ GU1-W02/BT06 Tinhchisocannangcuacothe/BT Tinhchisocannangcuacothe/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Tinhchisocannangcuacothe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // B1: Khai báo biến 
+            double height;
+            double weight;
+
+            // B2: Nhập dữ liệu
+            Console.WriteLine("Please enter a height");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your weight");
+            weight = float.Parse(Console.ReadLine());
+
+            // B3: Tính chỉ số theo công thức cho trước
+            double bmi = weight / Math.Pow(height, 2);
+            bmi = Math.Round(bmi, 1);
+
+            // B4: hiển thị chỉ số và phân loại kết quả
+            Console.Write("BMI: " + bmi);
+
+            if (bmi < 18)
+                Console.WriteLine(" Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine(" Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine(" Overweight");
+            else
+                Console.WriteLine(" Obese");
+        }
+
+    }
+}
---
+++ GU1-W02/BT07 UD doc so thanh chu/UD_docsothanhchu/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD_docsothanhchu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Tuoi;
+            Console.InputEncoding = Encoding.Unicode;       // Nhập vào định dạng Unicode
+            Console.OutputEncoding = Encoding.Unicode;      // Xuất ra màn hình định dạng Unicode
+            Console.Write("Nhập số muốn đọc: ");
+            Tuoi = Int32.Parse(Console.ReadLine());
+            switch (Chu)
+            {
+                case 1:
+                    Console.WriteLine("ONE");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+
+                
+            }
+          
+
+
+        }
+    }
+}
