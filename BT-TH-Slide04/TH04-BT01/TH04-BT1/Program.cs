c690d187a8bb7ced69a78449be3924f9e462471e
thuanhm2508 <thuanhm2508@gmail.com>
2023-10-19 01:01:20 +0700

    BT

---
+++ BT-TH-Slide04/TH04-BT01/TH04-BT1/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH04_BT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TH01();
+            //TH02();
+            //TH03();
+            //TH04();
+            //TH05();
+            //TH06();
+            //TH07();
+            TH08();
+        }
+        private static void TH01()
+        {
+            // Nhập 2 số và tính tổng. Xuất theo định dạng a + b = tổng
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            double a;
+            double b;
+            Console.WriteLine("Input a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input b: ");
+            b = double.Parse(Console.ReadLine());
+            double c = a + b;
+            Console.WriteLine("Kết quả {0} + {1} = {2}", a, b, c);
+        }
+        private static void TH02()
+        {
+            // Nhập 2 số nguyên N và M. Yêu cầu tính tổng chữ số cuối cùng của M và N
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            int m;
+            int n;
+            Console.Write("Input M: ");
+            m = int.Parse(Console.ReadLine());
+            Console.Write("Input N: ");
+            n = int.Parse(Console.ReadLine());
+            int k = (m % 10) + (n % 10);
+            Console.WriteLine("Tổng chữ số cuối cùng của M và N là: {0} + {1} = {2} ", m % 10, n % 10, k);
+        }
+        private static void TH03()
+        {
+            //Nhập 2 số a và b. Yêu cầu hoán vị giá trị của a và b, sau đó xuất kết quả ra màn hình.
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            int a;
+            int b;
+            Console.WriteLine("Input a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input b: ");
+            b = int.Parse(Console.ReadLine());
+            int swap = a;
+            a = b;
+            b = swap;
+            Console.WriteLine("Kết quả a là {0}", a);
+            Console.WriteLine("Kết quả b là {0}", b);
+        }
+        private static void TH04()
+        {
+            //Nhập 1 số có 2 chữ số. Yêu cầu xuất ra tổng các chữ số của nó ra màn hình
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+        nhaplaiso:
+            int input;
+            Console.Write("Input: ");
+            //input = int.Parse(Console.ReadLine());
+            if (int.TryParse(Console.ReadLine(), out input) && input >= 100 || input < 10)
+            {
+                Console.WriteLine("Số hoặc ký tự đã nhập không hợp lệ vui lòng nhập số có 2 chữ số.");
+                goto nhaplaiso;
+            }
+            else
+            {
+                int donvi = input % 10;
+                int nguyen = input / 10;
+                Console.WriteLine("Tổng các chữ số của số đã nhập là: {0} + {1} = {2}", nguyen, donvi, nguyen + donvi);
+            }
+        }
+        private static void TH05()
+        {
+            //Nhập 1 số gồm 3 chữ số, yêu cầu xuất ra tổng các chữ số của nó ra màn hình.
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+        nhaplaiso:
+            int input;
+            Console.Write("Input: ");
+            if (int.TryParse(Console.ReadLine(), out input) && input >= 999 || input <= 100)
+            {
+                Console.WriteLine("Số hoặc ký tự đã nhập không hợp lệ vui lòng nhập số có 3 chữ số.");
+                goto nhaplaiso;
+            }
+            else
+            {
+                int donvi = input % 10;
+                int chuc = (input % 100) / 10;
+                int tram = input / 100;
+                Console.WriteLine("Tổng các chữ số của số đã nhập là : {0} + {1} + {2} = {3}", tram, chuc, donvi, tram + chuc + donvi);
+            }
+        }
+        private static void TH06()
+        {
+            //Nhập vào 1 số gồm 4 chữ số, yêu cầu xuất số đó theo thứ tự ngược lại
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+        nhaplaiso:
+            int input;
+            Console.Write("Input: ");
+            if (int.TryParse(Console.ReadLine(), out input) && input > 9999 || input < 1000)
+            {
+                Console.WriteLine("Số hoặc ký tự không hợp lệ vui lòng nhập số có 4 chữ số.");
+                goto nhaplaiso;
+            }
+            else
+            {
+                int nghin = input / 1000;
+                int tram = (input / 100) % 10;
+                int chuc = (input / 10) % 10;
+                int donvi = (input % 10);
+                Console.WriteLine("Số đã nhập ngược lại là {0}{1}{2}{3}", donvi, chuc, tram, nghin);
+
+
+            }
+
+        }
+        private static void TH07()
+        {
+            //Nhập vào 2 ký tự, yêu cầu xuất ra mã ASCII của từng ký tự và tổng giá trị mã ASCII của 2 ký tự.
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            char input1;
+            char input2;
+            Console.Write("Input: ");
+            input1 = Convert.ToChar(Console.ReadLine());
+            Console.Write("Input: ");
+            input2 = Convert.ToChar(Console.ReadLine());
+            byte ascii = (byte)input1;
+            byte ascii2 = (byte)input2;
+            int x = ascii + ascii2;
+            Console.WriteLine("2 Ký tự bạn vừa nhập là {0} và {1} có giá trị theo bảng mã ASCII là {2} và {3}", input1, input2, ascii, ascii2);
+            Console.WriteLine("Tổng giá trị mã ASCII của 2 ký tự là: {0} + {1} = {2}", ascii, ascii2, x);
+            return;
+
+        }
+        private static void TH08()
+        {
+            //TH08 : Nhập vào 1 số thực. Xuất ra màn hình số lấy chính xác 3 chữ số thập phân từ ban đầu bằng 2 cách
+            //Cách 1: Sử dụng làm làm tròn.
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            /*
+            double sothuc;
+            Console.Write("Input: ");
+            sothuc = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Số {0} sau khi làm tròn ba chữ số là: {1}", sothuc, Math.Round(sothuc, 3));
+            */
+            // Cách 2: Sử dụng định dạng xuất
+            double sothuc;
+            Console.WriteLine("Input: ");
+            sothuc = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("Số {0} sau khi làm tròn ba chữ số là:", sothuc);
+
+        }
+
+    }
+}
--- GU1-W02/BT07 UD doc so thanh chu/UD_docsothanhchu/Program.cs
+++ GU1-W02/BT07 UD doc so thanh chu/UD_docsothanhchu/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace UD_docsothanhchu
 {
@@ -11,34 +13,212 @@ namespace UD_docsothanhchu
     {
         static void Main(string[] args)
         {
-            int Tuoi;
+            int a = -1;
+            int luotdem = 3;
             Console.InputEncoding = Encoding.Unicode;       // Nhập vào định dạng Unicode
             Console.OutputEncoding = Encoding.Unicode;      // Xuất ra màn hình định dạng Unicode
+            // Nhập số ..
             Console.Write("Nhập số muốn đọc: ");
-            Tuoi = Int32.Parse(Console.ReadLine());
-            switch (Chu)
+            //Console.Write(int.TryParse(Console.ReadLine(), out a));
+            bool docso = int.TryParse(Console.ReadLine(), out a);
+            if (docso)
             {
-                case 1:
-                    Console.WriteLine("ONE");
-                    break;
-                case 2:
-                    Console.WriteLine("Two");
-                    break;
-                case 3:
-                    Console.WriteLine("Three");
-                    break;
-                case 4:
-                    Console.WriteLine("Four");
-                    break;
-                case 5:
-                    Console.WriteLine("Five");
-                    break;
 
-                
+                if (docso && a >= 0 && a <= 99)
+                {
+                    if (a == 0)
+                    {
+                        Console.WriteLine("Zero");
+                        return;
+                    }
+                    if (a >= 0 && a < 10)
+                    {
+                        switch (a)
+                        {
+                            case 1: Console.WriteLine("one"); break;
+                            case 2: Console.WriteLine("two"); break;
+                            case 3: Console.WriteLine("three"); break;
+                            case 4: Console.WriteLine("four"); break;
+                            case 5: Console.WriteLine("five"); break;
+                            case 6: Console.WriteLine("six"); break;
+                            case 7: Console.WriteLine("seven"); break;
+                            case 8: Console.WriteLine("eight"); break;
+                            case 9: Console.WriteLine("nine"); break;
+
+                        }
+                    }
+                    if (a >= 11 && a < 20)
+                    {
+                        switch (a)
+                        {
+                            case 11: Console.WriteLine("eleven"); break;
+                            case 12: Console.WriteLine("twelve"); break;
+                            case 13: Console.WriteLine("thirteen"); break;
+                            case 14: Console.WriteLine("fourteen"); break;
+                            case 15: Console.WriteLine("fifteen"); break;
+                            case 16: Console.WriteLine("sixteen"); break;
+                            case 17: Console.WriteLine("seventeen"); break;
+                            case 18: Console.WriteLine("eighteen"); break;
+                            case 19: Console.WriteLine("nineteen"); break;
+                        }
+                    }
+                    if (a % 10 == 0)
+                    {
+                        switch (a)
+                        {
+                            case 10: Console.WriteLine("ten"); break;
+                            case 20: Console.WriteLine("twenty"); break;
+                            case 30: Console.WriteLine("thirty"); break;
+                            case 40: Console.WriteLine("fourty"); break;
+                            case 50: Console.WriteLine("fifty"); break;
+                            case 60: Console.WriteLine("sixty"); break;
+                            case 70: Console.WriteLine("seventy"); break;
+                            case 80: Console.WriteLine("eighty"); break;
+                            case 90: Console.WriteLine("ninety"); break;
+
+                        }
+                    }
+                    if (a > 20 && a % 10 != 0)
+                    {
+                        int hangdonvi;
+                        hangdonvi = a % 10;
+                        int hangchuc;
+                        hangchuc = a / 10;
+
+                        switch (hangchuc * 10)
+                        {
+                            case 10: Console.Write("ten "); break;
+                            case 20: Console.Write("twenty "); break;
+                            case 30: Console.Write("thirty "); break;
+                            case 40: Console.Write("fourty "); break;
+                            case 50: Console.Write("fifty "); break;
+                            case 60: Console.Write("sixty "); break;
+                            case 70: Console.Write("seventy "); break;
+                            case 80: Console.Write("eighty "); break;
+                            case 90: Console.Write("ninety "); break;
+                        }
+
+                        switch (hangdonvi)
+                        {
+                            case 1: Console.WriteLine("one"); break;
+                            case 2: Console.WriteLine("two"); break;
+                            case 3: Console.WriteLine("three"); break;
+                            case 4: Console.WriteLine("four"); break;
+                            case 5: Console.WriteLine("five"); break;
+                            case 6: Console.WriteLine("six"); break;
+                            case 7: Console.WriteLine("seven"); break;
+                            case 8: Console.WriteLine("eight"); break;
+                            case 9: Console.WriteLine("nine"); break;
+
+                        }
+                    }
+
+                }
+                if (docso && a >= 100 && a <= 999)
+                {
+                    int hangtram;
+                    hangtram = a / 100;
+                    int hangchucvahangdonvi = a % 100;
+                    //Console.WriteLine(hangchucvahangdonvi);
+                    switch (hangtram)
+                    {
+                        case 1: Console.Write("One hundred "); break;
+                        case 2: Console.Write("Two hundred "); break;
+                        case 3: Console.Write("Three hundred "); break;
+                        case 4: Console.Write("Four hundred"); break;
+                        case 5: Console.Write("Five hundred "); break;
+                        case 6: Console.Write("Six hundred "); break;
+                        case 7: Console.Write("Seven hundred "); break;
+                        case 8: Console.Write("Eight hundred "); break;
+                        case 9: Console.Write("Nine hundred "); break;
+                    }
+                    if (a % 100 != 0)
+                    {
+                        Console.Write("and ");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+                    switch (hangchucvahangdonvi)
+                    {
+                        case 1: Console.WriteLine("one"); break;
+                        case 2: Console.WriteLine("two"); break;
+                        case 3: Console.WriteLine("three"); break;
+                        case 4: Console.WriteLine("four"); break;
+                        case 5: Console.WriteLine("five"); break;
+                        case 6: Console.WriteLine("six"); break;
+                        case 7: Console.WriteLine("seven"); break;
+                        case 8: Console.WriteLine("eight"); break;
+                        case 9: Console.WriteLine("nine"); break;
+
+                    }
+                    switch (hangchucvahangdonvi)
+                    {
+                        case 11: Console.WriteLine("eleven"); break;
+                        case 12: Console.WriteLine("twelve"); break;
+                        case 13: Console.WriteLine("thirteen"); break;
+                        case 14: Console.WriteLine("fourteen"); break;
+                        case 15: Console.WriteLine("fifteen"); break;
+                        case 16: Console.WriteLine("sixteen"); break;
+                        case 17: Console.WriteLine("seventeen"); break;
+                        case 18: Console.WriteLine("eighteen"); break;
+                        case 19: Console.WriteLine("nineteen"); break;
+                    }
+                    switch (hangchucvahangdonvi)
+                    {
+                        case 10: Console.WriteLine("ten"); break;
+                        case 20: Console.WriteLine("twenty"); break;
+                        case 30: Console.WriteLine("thirty"); break;
+                        case 40: Console.WriteLine("fourty"); break;
+                        case 50: Console.WriteLine("fifty"); break;
+                        case 60: Console.WriteLine("sixty"); break;
+                        case 70: Console.WriteLine("seventy"); break;
+                        case 80: Console.WriteLine("eighty"); break;
+                        case 90: Console.WriteLine("ninety"); break;
+
+                    }
+                    int hangdonvi;
+                    hangdonvi = hangchucvahangdonvi % 10;
+                    int hangchuc;
+                    hangchuc = hangchucvahangdonvi / 10;
+
+                    switch (hangchuc * 10)
+                    {
+                        case 10: Console.Write("ten "); break;
+                        case 20: Console.Write("twenty "); break;
+                        case 30: Console.Write("thirty "); break;
+                        case 40: Console.Write("fourty "); break;
+                        case 50: Console.Write("fifty "); break;
+                        case 60: Console.Write("sixty "); break;
+                        case 70: Console.Write("seventy "); break;
+                        case 80: Console.Write("eighty "); break;
+                        case 90: Console.Write("ninety "); break;
+                    }
+
+                    switch (hangdonvi)
+                    {
+                        case 1: Console.WriteLine("one"); break;
+                        case 2: Console.WriteLine("two"); break;
+                        case 3: Console.WriteLine("three"); break;
+                        case 4: Console.WriteLine("four"); break;
+                        case 5: Console.WriteLine("five"); break;
+                        case 6: Console.WriteLine("six"); break;
+                        case 7: Console.WriteLine("seven"); break;
+                        case 8: Console.WriteLine("eight"); break;
+                        case 9: Console.WriteLine("nine"); break;
+
+                    }
+                }
             }
-          
-
+            else
+            {
+                Console.WriteLine("Nhập sai ký tự...");
+                return;
+            }
 
         }
     }
 }
+
+
