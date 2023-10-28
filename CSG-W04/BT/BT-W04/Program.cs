f10f48c5ead06e58ce4868787340cb03d9abad27
thuanhm2508 <thuanhm2508@gmail.com>
2023-10-28 15:27:07 +0700

    BT1
    Sum PT

---
+++ CSG-W04/BT/BT-W04/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace BT_W04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C1();
+            //C2();
+        }
+        private static void C1()
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+
+            int total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total += numbers[i];
+            }
+            Console.WriteLine(total.ToString());
+            Console.ReadKey();
+        }
+        private static void C2()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            List<int> lst = new List<int>();
+            lst.Add(2);
+            lst.Add(5);
+            lst.Add(9);
+            lst.Add(6);
+            lst.Add(7);
+            Console.WriteLine("Tổng các phần tử trong chuỗi là: " + lst.Sum());
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ CSG-W04/GW04-Collection/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;                              //Linq: 
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //baitest();
+            baitestlinq();
+
+        }
+        private static void baitest()
+        {
+            ArrayList no_lst = new ArrayList();
+            no_lst.Add(1);
+            no_lst.Add("A B C");
+            no_lst.Add(true);
+
+            foreach (var item in no_lst)            //Sử dụng biến var và object khi không xác định kiểu dữ liệu.
+            {
+                Console.WriteLine(item + " ");
+
+            }
+            Console.ReadKey();
+        }
+        private static void baitestlinq()
+        {
+            List<int> lst = new List<int>();
+            int n = 10;
+            Random rd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                lst.Add(i);
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            foreach (int item in lst)
+            {
+                if (item % 2 == 1)
+                    Console.Write(item * 2 + " ");
+                else Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            
+
+
+
+
+        }
+    }
+}
