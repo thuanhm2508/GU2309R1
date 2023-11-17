083d688b206627c26db1b7c7b47aa59a9948fc12
thuanhm2508 <thuanhm2508@gmail.com>
2023-11-18 00:30:57 +0700

    D
    D

---
+++ GU1-W05/BT/timgiatrinhonhattrongmangsudungphuongthuc/Finminarr/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finminarr
+{
+    class Program
+    {
+        public static int MinValue(int[] array)
+        {
+
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+            int index = MinValue(arr);
+            Console.WriteLine("The smallest element in the array is: " + arr[index]);
+        }
+    }
+}
