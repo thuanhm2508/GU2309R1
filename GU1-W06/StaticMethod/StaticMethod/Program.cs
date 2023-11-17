c70079792d3a5203e9c7bdbc1c4d99614070fed2
thuanhm2508 <thuanhm2508@gmail.com>
2023-11-18 01:21:41 +0700

    Static Method
    GW6

---
+++ GU1-W06/StaticMethod/StaticMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student.Change();//calling change method
+                             //creating objects
+            Student s1 = new Student(111, "Hoang");
+            Student s2 = new Student(222, "Minh");
+            Student s3 = new Student(333, "Thuan");
+            //calling display method
+            s1.Display();
+            s2.Display();
+            s3.Display();
+        }
+    }
+}
