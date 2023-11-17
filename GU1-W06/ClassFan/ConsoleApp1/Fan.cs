d3def03dffb8d2cdb9345cd526e221d3db60b778
thuanhm2508 <thuanhm2508@gmail.com>
2023-11-18 01:26:08 +0700

    ClassFan
    GW6

---
+++ GU1-W06/ClassFan/ConsoleApp1/Fan.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Fan
+    {
+        // Hằng số định nghĩa tốc độ quạt
+        public const int SLOW = 1;
+        public const int MEDIUM = 2;
+        public const int FAST = 3;
+
+        // Trường
+        private int speed;
+        private bool on;
+        private double radius;
+        private string color;
+
+        // Getter và setter cho các thuộc tính
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public bool On
+        {
+            get { return on; }
+            set { on = value; }
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        // Phương thức khởi tạo không tham số
+        public Fan()
+        {
+            speed = SLOW;
+            on = false;
+            radius = 5;
+            color = "blue";
+        }
+
+        // Phương thức ToString
+        public override string ToString()
+        {
+            if (on)
+            {
+                return $"Speed: {speed}, Color: {color}, Radius: {radius} - Fan is on";
+            }
+            else
+            {
+                return $"Color: {color}, Radius: {radius} - Fan is off";
+            }
+        }
+    }
+}
---
+++ GU1-W06/ClassFan/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tạo đối tượng Fan 1
+            Fan fan1 = new Fan();
+            fan1.Speed = Fan.FAST;
+            fan1.Radius = 10;
+            fan1.Color = "yellow";
+            fan1.On = true;
+
+            // Tạo đối tượng Fan 2
+            Fan fan2 = new Fan();
+            fan2.Speed = Fan.MEDIUM;
+            fan2.Radius = 5;
+            fan2.Color = "blue";
+            fan2.On = false;
+
+            // Hiển thị thông tin đối tượng
+            Console.WriteLine("Fan 1: " + fan1.ToString());
+            Console.WriteLine("Fan 2: " + fan2.ToString());
+        }
+    }
+}
