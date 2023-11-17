e023e5e48d776b3c12e355bcba27ed746905e9cc
thuanhm2508 <thuanhm2508@gmail.com>
2023-11-18 01:41:53 +0700

    F
    sS

---
+++ GU1-W06/StopWatch/Stopwatch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            StopWatch stopwatch = new StopWatch();
+
+            int[] arrayToSort = new int[100000];
+            Random random = new Random();
+            for (int i = 0; i < arrayToSort.Length; i++)
+            {
+                arrayToSort[i] = random.Next(1, 1000);
+            }
+
+            stopwatch.Start();
+
+            for (int i = 0; i < arrayToSort.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arrayToSort.Length; j++)
+                {
+                    if (arrayToSort[j] < arrayToSort[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = arrayToSort[i];
+                arrayToSort[i] = arrayToSort[minIndex];
+                arrayToSort[minIndex] = temp;
+            }
+
+            stopwatch.Stop();
+
+            double elapsedTime = stopwatch.GetElapsedTime();
+            Console.WriteLine($"Elapsed Time for Selection Sort: {elapsedTime} milliseconds");
+                      
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GU1-W06/StopWatch/Stopwatch/StopWatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    public class StopWatch
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public DateTime StartTime
+        {
+            get { return startTime; }
+        }
+
+        public DateTime EndTime
+        {
+            get { return endTime; }
+        }
+
+        public StopWatch()
+        {
+            startTime = DateTime.Now;
+        }
+
+        public void Start()
+        {
+            startTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            endTime = DateTime.Now;
+        }
+
+        public double GetElapsedTime()
+        {
+            TimeSpan elapsed = endTime - startTime;
+            return elapsed.TotalMilliseconds;
+        }
+    }
+}
