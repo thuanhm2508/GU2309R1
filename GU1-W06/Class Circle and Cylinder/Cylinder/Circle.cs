3e0faaf12ecf60b52680415fdb9127205d2d68f8
thuanhm2508 <thuanhm2508@gmail.com>
2023-11-24 04:25:45 +0700

    GU01-W06
    Circle and Cylinder

---
+++ GU1-W06/Class Circle and Cylinder/Cylinder/Circle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cylinder
+{
+    public class Circle
+    {
+        private double radius;
+        private string color;
+        public Circle(double radius, string color)
+        {
+            this.radius = radius;
+            this.color = color;
+        }
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public double Area
+        {
+            get { return Math.PI * radius * radius; }
+        }
+        public override string ToString()
+        {
+            return $"Circle [radius={radius}, color={color}]";
+        }
+    }
+
+    // Lớp Cylinder kế thừa từ lớp Circle
+    public class Cylinder : Circle
+    {
+        private double height;
+        public Cylinder(double radius, string color, double height) : base(radius, color)
+        {
+            this.height = height;
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public double Volume
+        {
+            get { return base.Area * height; }
+        }
+        // Phương thức ToString
+        public override string ToString()
+        {
+            return $"Cylinder [radius={Radius}, color={Color}, height={height}]";
+        }
+    }
+
+}
---
+++ GU1-W06/Class Circle and Cylinder/Cylinder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cylinder
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Tạo một đối tượng Circle
+            Circle circle = new Circle(5.0, "Red");                 
+            Console.WriteLine("Circle Information:");
+            Console.WriteLine(circle);
+            Console.WriteLine($"Area: {circle.Area}");
+            Console.WriteLine();
+
+            // Tạo một đối tượng Cylinder
+            Cylinder cylinder = new Cylinder(3.0, "Blue", 8.0);                  
+            Console.WriteLine("Cylinder Information:");
+            Console.WriteLine(cylinder);
+            Console.WriteLine($"Volume: {cylinder.Volume}");
+        }
+    }
+}
