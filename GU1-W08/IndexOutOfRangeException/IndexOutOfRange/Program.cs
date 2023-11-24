c6c0dd44fc8b0025565f996746e0fad132b9137d
thuanhm2508 <thuanhm2508@gmail.com>
2023-11-24 21:16:32 +0700

    TryCatch
    DA1

---
+++ GU1-W08/IndexOutOfRangeException/IndexOutOfRange/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOutOfRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+
+        }
+    }
+}
