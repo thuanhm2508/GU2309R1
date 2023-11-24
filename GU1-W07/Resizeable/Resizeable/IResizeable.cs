78aed17d36a822df441148bda45d06811fcfb5d8
thuanhm2508 <thuanhm2508@gmail.com>
2023-11-24 09:49:50 +0700

    UPD
    UPD

---
+++ GU1-W07/Resizeable/Resizeable/IResizeable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resizeable
+{
+    public interface IResizeable
+    {
+        public void Resize(double percent);    
+    }
+}
---
+++ GU1-W07/Resizeable/Resizeable/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resizeable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Shape Test
+            //Shape shape = new Shape();
+            //Console.WriteLine(shape);
+
+            //shape = new Shape("red", false);
+            //Console.WriteLine(shape);
+
+            //// Circle Test
+            //Circle circle = new Circle();
+            //Console.WriteLine(circle);
+
+            //circle = new Circle(3.5);
+            //Console.WriteLine(circle);
+
+            //circle = new Circle(3.5, "indigo", false);
+            //Console.WriteLine(circle);
+
+            // Square Test
+            Square square = new Square();
+            Console.WriteLine(square);
+            square.Resize(0.5);
+            Console.WriteLine(square);
+
+            //square = new Square(2.3);
+            //Console.WriteLine(square);
+
+            //square = new Square(5.8, "yellow", true);
+            //Console.WriteLine(square);
+
+
+        }
+    }
+}
