2578dec05a9c758432ae6db45aab817a3367bf63
thuanhm2508 <thuanhm2508@gmail.com>
2023-11-24 09:25:16 +0700

    UPD
    PD

---
+++ GU1-W07/Icolorable/Icolorable/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Icolorable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Shape Test
+            //Shape shape = new Shape();
+            //Console.WriteLine(shape);
+
+            //shape = new Shape("red", false);
+            //Console.WriteLine(shape);
+
+            //// Circle Test
+            //Circle circle = new Circle();
+            //Console.WriteLine(circle);
+
+            //circle = new Circle(3.5);
+            //Console.WriteLine(circle);
+
+            //circle = new Circle(3.5, "indigo", false);
+            //Console.WriteLine(circle);
+
+            //// Square Test
+            //Square square = new Square();
+            //Console.WriteLine(square);
+
+            //square = new Square(2.3);
+            //Console.WriteLine(square);
+
+            //square = new Square(5.8, "yellow", true);
+            //Console.WriteLine(square);
+
+
+            Shape[] shapes = new Shape[3];
+
+            shapes[0] = new Circle(3.5, "indigo", false);
+            shapes[1] = new Rectangle(5, 7.5, "indigo", false);
+            shapes[2] = new Square(5.8, "yellow", true);
+
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine(shape);
+
+                Square square = shape as Square;
+                if (square != null)
+                {
+                    square.HowToColor();
+                }
+            }
+        }
+    }
+}
+
---
+++ GU1-W07/Icolorable/Icolorable/Shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icolorable
+{
+    public class Shape : IColorable
+    {
+        private string color = "green";
+        private bool filled = true;
+        public Shape() { }
+        public Shape(string color, bool filled)
+        {
+            this.color = color;
+            this.filled = filled;
+        }
+        public string GetColor()
+        {
+            return color;
+        }
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+        public bool IsFilled()
+        {
+            return filled;
+        }
+        public void SetFilled(bool filled)
+        {
+            this.filled = filled;
+        }
+        public virtual string ToString()
+        {
+            return "A Shape with color of "
+                + GetColor()
+                + " and "
+                + (IsFilled() ? "filled" : "not filled");
+        }
+        public void HowToColor()
+        {
+            if (filled)
+                Console.WriteLine($"Color of shape is: {GetColor()}");
+            else
+            {
+                Console.WriteLine("Color of shape is: nothing");
+            }
+
+        }
+    }
+}
